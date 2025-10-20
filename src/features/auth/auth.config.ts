47989d2b3605429f89a23df935b7e28c28520cf4
r4sheed <25135303+r4sheed@users.noreply.github.com>
2025-10-20 15:54:24 +0200

    Add Google and GitHub OAuth providers to auth config

--- src/features/auth/auth.config.ts
+++ src/features/auth/auth.config.ts
@@ -1,6 +1,8 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { type NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 
 import { verifyUserCredentials } from '@/features/auth/data/user';
 import { loginSchema } from '@/features/auth/schemas';
@@ -10,6 +12,14 @@ import { db } from '@/lib/prisma';
 export const authConfig = {
   adapter: PrismaAdapter(db),
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const parsed = loginSchema.safeParse(credentials);
