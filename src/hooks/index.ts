422f62fccbce709c0768e85fe2889643eff534ca
r4sheed <25135303+r4sheed@users.noreply.github.com>
2025-10-22 09:04:14 +0200

    Fix exports from hooks index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,3 @@
-export {
-  useAction,
-  isStatusSuccess,
-  isStatusError,
-  isStatusPending,
-  isStatusPartial,
-} from './use-action';
+export { useAction } from './use-action';
 export { useLayout } from './use-layout';
 export { useMetaColor } from './use-meta-color';
