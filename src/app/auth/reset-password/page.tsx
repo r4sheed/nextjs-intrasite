fc966eb4cc491866d087e49733c700f0655fd493
r4sheed <25135303+r4sheed@users.noreply.github.com>
2025-10-29 12:57:27 +0100

    Fix page component name

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,7 +1,7 @@
 import { AuthWrapper } from '@/features/auth/components/auth-wrapper';
 import { ResetForm } from '@/features/auth/components/reset-form';
 
-export default function VerifyEmailPage() {
+export default function ResetPasswordPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-4xl">
