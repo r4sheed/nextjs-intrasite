475057daaba9b29ea03c1752f83b600921f844fc
r4sheed <25135303+r4sheed@users.noreply.github.com>
2025-10-30 07:49:46 +0100

    Adjust max width of verify email form container
    Changed the container's max width on medium screens from 3xl to xl to better fit the layout on the verify email page.

--- src/app/auth/verify-email/page.tsx
+++ src/app/auth/verify-email/page.tsx
@@ -4,7 +4,7 @@ import { EmailVerificationForm } from '@/features/auth/components/verification-f
 export default function VerifyEmailPage() {
   return (
     <div className="bg-muted flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
+      <div className="w-full max-w-sm md:max-w-xl">
         <AuthWrapper>
           <EmailVerificationForm />
         </AuthWrapper>
