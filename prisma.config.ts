f33f822738a748da2abeffc329062c3cfecb16dc
r4sheed <25135303+r4sheed@users.noreply.github.com>
2025-11-05 15:49:54 +0100

    Update Prisma seed script path
    Changed the Prisma seed script path from 'src/prisma/seed.ts' to 'prisma/seed.ts' in both package.json and prisma.config.ts for consistency and correct script execution.

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,7 +7,7 @@ export default {
   schema: path.join('prisma', 'schema.prisma'),
   migrations: {
     path: path.join('prisma', 'migrations'),
-    seed: `tsx src/prisma/seed.ts`,
+    seed: `tsx prisma/seed.ts`,
   },
   views: {
     path: path.join('prisma', 'views'),
