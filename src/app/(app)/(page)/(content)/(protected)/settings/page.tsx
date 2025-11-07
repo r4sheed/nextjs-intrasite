286fc94ff0afdee53be9e64afeb0eae8f32d432f
r4sheed <25135303+r4sheed@users.noreply.github.com>
2025-11-07 20:01:35 +0100

    Remove settings page content and return null
    All content and logic from the settings page has been removed, and the component now returns null. This may be in preparation for a redesign or deprecation of the settings page.

--- src/app/(app)/(page)/(content)/(protected)/settings/page.tsx
+++ src/app/(app)/(page)/(content)/(protected)/settings/page.tsx
@@ -1,23 +1,3 @@
-import { Button } from '@/components/ui/button';
-
-import { auth, signOut } from '@/features/auth/lib/auth';
-
 export default async function SettingsPage() {
-  const session = await auth();
-  return (
-    <div>
-      <div className="mb-6 rounded-md p-4">
-        <pre>{JSON.stringify(session, null, 2)}</pre>
-      </div>
-      <form
-        action={async () => {
-          'use server';
-
-          await signOut();
-        }}
-      >
-        <Button type="submit">Sign Out</Button>
-      </form>
-    </div>
-  );
+  return null;
 }
